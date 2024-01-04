cdf2db1f93110b72fa3462a76f11a87febc62715
borolekrish <borolekrish1@gmail.com>
2024-01-04 22:50:24 +0530

    website completed

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Hero.css'
 import { HiLocationMarker } from 'react-icons/hi'
 import CountUp from 'react-countup'
+import { motion } from 'framer-motion'
+
 
 const hero = () => {
     return (
@@ -11,9 +13,15 @@ const hero = () => {
                 <div className='flexColStart hero-left'>
                     <div className="hero-tittle">
                         <div className="orange-circle" />
-                        <h1>
-                            Discover <br /> Most Suitable <br /> Property
-                        </h1>
+                        <motion.h1
+                            initial={{ y: '2rem', opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 2, type: 'spring' }}
+                        >
+                            Discover <br />
+                            Most Suitable <br />
+                            Home Property
+                        </motion.h1>
                     </div>
                     <div className="flexColStart hero-des">
                         <span className='secondaryText'>Find a Variety of prooperties that suit you very easily</span>
@@ -50,9 +58,13 @@ const hero = () => {
                 </div>
                 {/* right side */}
                 <div className="flexCenter hero-right">
-                    <div className="image-container">
+                    <motion.div className="image-container"
+                        initial={{ x: '7rem', opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 2, type: 'spring' }}
+                    >
                         <img src="./hero-image.png" alt="hero" />
-                    </div>
+                    </motion.div>
                 </div>
             </div >
         </section >
